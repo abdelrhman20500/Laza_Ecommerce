bd28ce830f5954e9ca62bf32d97df24bf1d156da
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2025-02-04 20:50:05 +0200

    Create Use Case For Product Details

--- lib/Features/product_details/domain/repo/product_details_repo.dart
+++ lib/Features/product_details/domain/repo/product_details_repo.dart
@@ -2,6 +2,6 @@ import 'package:dartz/dartz.dart';
 import '../../../../Core/utils/failure.dart';
 import '../../../home/presentation/view/home_tab/domain/entities/product_entity.dart';
 
-abstract class ProductCategoryByIdRepo{
+abstract class ProductDetailsRepo{
   Future<Either<Failure, List<ProductEntity>>> getProductDetailsRepo({required String token ,required String productId});
 }
