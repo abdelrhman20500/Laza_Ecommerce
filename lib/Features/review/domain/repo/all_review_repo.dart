db67165316133ec7bff5c91889054182309fc427
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2025-02-05 22:48:56 +0200

    Create Ues Case For Get All Reviews

--- lib/Features/review/domain/repo/all_review_repo.dart
+++ lib/Features/review/domain/repo/all_review_repo.dart
@@ -3,5 +3,5 @@ import 'package:laza_e_commerce/Core/utils/failure.dart';
 import 'package:laza_e_commerce/Features/review/domain/entities/review_entity.dart';
 
 abstract class AllReviewsRepo{
-  Future<Either<Failure, List<ReviewEntity>>> getAllReviews();
+  Future<Either<Failure, List<ReviewEntity>>> getAllReviews({required String token, required String productId});
 }
---
+++ lib/Features/review/domain/use_case/all_reviews_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:laza_e_commerce/Core/use-cases/use_case.dart';
+import 'package:laza_e_commerce/Core/utils/failure.dart';
+import 'package:laza_e_commerce/Core/utils/shared_preferences.dart';
+import 'package:laza_e_commerce/Features/review/domain/entities/review_entity.dart';
+import 'package:laza_e_commerce/Features/review/domain/repo/all_review_repo.dart';
+
+class AllReviewsUseCase extends UseCase<List<ReviewEntity>, String>{
+  final AllReviewsRepo allReviewsRepo;
+
+  AllReviewsUseCase(this.allReviewsRepo);
+  @override
+  Future<Either<Failure, List<ReviewEntity>>> call([String? productId])async{
+    return await allReviewsRepo.getAllReviews(token: SharedPref.getToken().toString(), productId: productId!);
+  }
+
+}
