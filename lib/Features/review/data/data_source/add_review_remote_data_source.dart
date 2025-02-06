3d043bdb0681fc2445ded199a61a790f2642282d
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2025-02-06 22:55:31 +0200

    Create Add Review Remote Data Source

---
+++ lib/Features/review/data/data_source/add_review_remote_data_source.dart
@@ -0,0 +1,32 @@
+import 'package:laza_e_commerce/Core/functions/api_service.dart';
+import 'package:laza_e_commerce/Core/utils/shared_preferences.dart';
+import '../../domain/entities/review_entity.dart';
+import '../model/review_model.dart';
+
+abstract class AddReviewRemoteDateSource{
+  Future<ReviewEntity> addReview(
+      {required String productId,
+        required String name,
+        required String feedback,
+        required double rate});
+}
+
+class AddReviewRemoteDataSourceImpl extends AddReviewRemoteDateSource{
+  final ApiService apiService;
+
+  AddReviewRemoteDataSourceImpl(this.apiService);
+  @override
+  Future<ReviewEntity> addReview({required String productId, required String name,
+    required String feedback, required double rate}) async{
+    final response = await apiService.post(
+        'Review/AddReview/$productId',
+        {
+          "Username": name,
+          "Feedback": feedback,
+          "Rating": rate,
+        },
+        token: SharedPref.getToken());
+
+    return ReviewModel.fromJson(response.data as Map<String, dynamic>);
+  }
+}
